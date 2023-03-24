d6d10900486b385ce847fff393e0cc4a72cfeb81
kkapchin <kkapchin@yahoo.com>
2023-03-24 01:23:11 -0400

    NavLinks for navbar items

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,12 +1,15 @@
+import {NavLink} from "react-router-dom";
+import {AppRoute} from "../../const";
+
 export default function Navbar() {
     return (
         <div className="navbar__container">
             <ul className="navbar">
-                <li className="active nav__item">
-                    <a href="/" >Home</a>
+                <li className="nav__item">
+                    <NavLink to={AppRoute.Main} >Home</NavLink>
                 </li>
                 <li className="nav__item">
-                    <a href="/" >Messenger</a>
+                    <NavLink to={AppRoute.Messenger} >Messenger</NavLink>
                 </li>
                 <li className="nav__item">
                     <a href="/" >Photos</a>
