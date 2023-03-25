29c934efcd95325eb643556e7da234dfd0d748df
kkapchin <kkapchin@yahoo.com>
2023-03-25 00:21:07 -0400

    chat-list markup

--- src/components/chat-list/chat-list.tsx
+++ src/components/chat-list/chat-list.tsx
@@ -2,7 +2,15 @@ export default function ChatList() {
     return (
         <div className="chat-list__wrap">
             <div className="chat-list__container">
-                <h6 className="title">Chat List</h6>
+                <div className="chat-list__item">
+                    <h6 className="title">Bloody d0b1k</h6>
+                </div>
+                <div className="chat-list__item active">
+                    <h6 className="title">Madara Alexandrovich</h6>
+                </div>
+                <div className="chat-list__item">
+                    <h6 className="title">Dedist Drebedist</h6>
+                </div>
             </div>
         </div>
     );
