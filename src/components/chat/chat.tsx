c9dde2dce6fdf6f64c5bcee6f8c145908f132d4c
kkapchin <kkapchin@yahoo.com>
2023-03-25 00:25:42 -0400

    chat init markup

--- src/components/chat/chat.tsx
+++ src/components/chat/chat.tsx
@@ -1,7 +1,9 @@
 export default function Chat () {
     return (
         <div className="chat__wrap">
-            <h6 className="title">Select a chat to start messaging</h6>
+            <div className="chat__container">
+                <h6 className="title">Select a chat to start messaging</h6>
+            </div>
         </div>
     );
 }
