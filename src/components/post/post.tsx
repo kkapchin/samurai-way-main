f2103fb031212412f3d2975f8ad90de7ecd6e92f
kkapchin <kkapchin@yahoo.com>
2023-03-28 23:11:46 -0400

    Post Component refactor

--- src/components/post/post.tsx
+++ src/components/post/post.tsx
@@ -1,17 +1,23 @@
-export default function Post() {
+import {PostType} from "../../types/post-type";
+
+type PostPropsType = {
+    post: PostType
+}
+
+export default function Post({ post } : PostPropsType) {
     return (
         <div className="post__container grad-border">
             <div className="post__content grad-bordered">
                 <div className="post__author">
                     <div className="d__flex align__c">
-                        <img className="post-author__avatar" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRF_-_Xc8u7HQEVA66YYznJZwFctc06g3wFhKajU_BAVZb1BiFXBd8qChGfjK8WBkfALq0&usqp=CAU" alt="profile-avatar"></img>
+                        <img className="post-author__avatar" src={post.user.avatar} alt="profile-avatar"></img>
                         <div className="post-author__info">
                             <h6 className="title">
                                 <a href="/">
-                                    <span>Ivo Namtar Wijers</span>
+                                    <span>{post.user.name}</span>
                                 </a>
                             </h6>
-                            <time className="post__time">1 min ago</time>
+                            <time className="post__time">{post.time}</time>
                         </div>
                     </div>
                     <span className="post-edit__btn">
@@ -21,10 +27,7 @@ export default function Post() {
                     </span>
                 </div>
                 <div className="post__text">
-                    Lorem ipsum dolor sit amet. Non recusandae quidem et velit quidem qui iste animi et mollitia repudiandae aut dolorem voluptas est molestiae quisquam et voluptas aperiam. At sint reiciendis hic ipsam reiciendis et quia quod.
-                    Lorem ipsum dolor sit amet. Non recusandae quidem et velit quidem qui iste animi et mollitia repudiandae aut dolorem voluptas est molestiae quisquam et voluptas aperiam. At sint reiciendis hic ipsam reiciendis et quia quod.
-                    Lorem ipsum dolor sit amet. Non recusandae quidem et velit quidem qui iste animi et mollitia repudiandae aut dolorem voluptas est molestiae quisquam et voluptas aperiam. At sint reiciendis hic ipsam reiciendis et quia quod.
-                    Lorem ipsum dolor sit amet. Non recusandae quidem et velit quidem qui iste animi et mollitia repudiandae aut dolorem voluptas est molestiae quisquam et voluptas aperiam. At sint reiciendis hic ipsam reiciendis et quia quod.
+                    {post.text}
                 </div>
                 <div className="like__btn-active"></div>
             </div>
